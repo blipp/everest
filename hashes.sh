bae2fdef4ebd269a5feeab3ac8416a69f3fd87e1
Aseem Rastogi <aseemr@microsoft.com>
2021-02-01 15:01:37 +0530

    update hashes

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=5d0a057a4b4af4b50406c1a0ed4efafa328cb6fd
-branches[FStar]=master
-hashes[hacl-star]=96ad4e4574bf71f4398d18d1fabae645b258b04f
-branches[hacl-star]=master
-hashes[kremlin]=637dae2315480ee2f892cbad9fd238f798a38381
-branches[kremlin]=master
+hashes[FStar]=c147cb21a2a6e26116d9c7ab30afb841f93fc494
+branches[FStar]=_aseem_1916
+hashes[hacl-star]=20683462a30b51aa0f3fbdb04ef718b24164dc09
+branches[hacl-star]=_aseem_1916
+hashes[kremlin]=803cc9499842e21b2359c1ccf00c7876ad762733
+branches[kremlin]=_aseem_fstar_1916
 hashes[mitls-fstar]=cd5edf3c8cddfc234da9d1928cd632e0efea6a10
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[quackyducky]=06072a6cbdb2b266a8a754f6d19981301e80c11e
-branches[quackyducky]=master
+hashes[quackyducky]=f1d93556d2f719dc3772b58014719f8da7b9d90f
+branches[quackyducky]=_aseem_1916
