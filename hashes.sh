b6891671da5177de09560052be79581458322749
Aseem Rastogi <aseemr@microsoft.com>
2021-02-02 15:12:39 +0530

    update hashes

--- hashes.sh
+++ hashes.sh
@@ -1,8 +1,8 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=c147cb21a2a6e26116d9c7ab30afb841f93fc494
-branches[FStar]=_aseem_1916
-hashes[hacl-star]=20683462a30b51aa0f3fbdb04ef718b24164dc09
+hashes[FStar]=ebfba47568c8a7a7ac054db56210022d7f7eca76
+branches[FStar]=_aseem_1916_no_lex_eq
+hashes[hacl-star]=e353b3a8a44665229468613e233efc3553252638
 branches[hacl-star]=_aseem_1916
 hashes[kremlin]=803cc9499842e21b2359c1ccf00c7876ad762733
 branches[kremlin]=_aseem_fstar_1916
